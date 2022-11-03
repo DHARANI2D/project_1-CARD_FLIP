b9a58218647610380b7a1beb22ca6c104bda6b99
DHARANIDHARAN SENTHILKUMAR <99940501+DHARANI2D@users.noreply.github.com>
2022-11-03 11:30:30 +0530

    project 1-card flip

---
+++ flip.js
@@ -0,0 +1,9 @@
+<script>
+var cards = document.querySelectorAll('.card');
+
+[...cards].forEach((card)=>{
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  });
+});
+</script>
